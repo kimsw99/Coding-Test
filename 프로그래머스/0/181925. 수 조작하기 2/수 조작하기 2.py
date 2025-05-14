c24579141df495696d456ced5153ce8bce1450ce
kimsw99 <kseonguk99@gmail.com>
2025-05-15 08:57:35 +0900

    [level 0] Title: 수 조작하기 2, Time: 13.94 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,12 +1,23 @@
+# def solution(numLog):
+#     answer = ''
+#     for i in range(len(numLog))[1:]:
+#         if  numLog[i] - numLog[i-1] == 1 :
+#             answer += "w"
+#         elif numLog[i] - numLog[i-1] == -1:
+#             answer += "s"
+#         elif numLog[i] - numLog[i-1] == 10:
+#             answer += "d"
+#         elif numLog[i] - numLog[i-1] == -10:
+#             answer += "a"
+#     return answer
+
 def solution(numLog):
-    answer = ''
-    for i in range(len(numLog))[1:]:
-        if  numLog[i] - numLog[i-1] == 1 :
-            answer += "w"
-        elif numLog[i] - numLog[i-1] == -1:
-            answer += "s"
-        elif numLog[i] - numLog[i-1] == 10:
-            answer += "d"
-        elif numLog[i] - numLog[i-1] == -10:
-            answer += "a"
-    return answer
+    control_map = {
+        1: 'w',
+        -1: 's',
+        10: 'd',
+        -10: 'a'
+    }
+    
+    return ''.join(control_map[numLog[i] - numLog[i - 1]] for i in 
+                   range(1, len(numLog)))
