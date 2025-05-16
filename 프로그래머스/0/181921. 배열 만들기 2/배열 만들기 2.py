945dd71c12f18cff97b903c82ddac7e844459dd9
kimsw99 <kseonguk99@gmail.com>
2025-05-16 18:34:30 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.07 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,14 @@
+def solution(l, r):
+    from collections import deque
+    result = []
+    queue = deque(['5'])
+    while queue:
+        current = queue.popleft()
+        num = int(current)
+        if num > r:
+            continue  
+        if l <= num <= r:
+            result.append(num)
+        queue.append(current + '0')
+        queue.append(current + '5')
+    return sorted(result) if result else [-1]
